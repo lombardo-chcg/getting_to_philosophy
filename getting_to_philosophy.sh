df0e9acfc1ef5b0a7584bd7a49635a9695149b80
lombardo-chcg <dpa.lombardo@gmail.com>
2017-02-05 14:16:19 -0600

    mvp

--- getting_to_philosophy.sh
+++ getting_to_philosophy.sh
@@ -4,27 +4,43 @@
 # Ignoring external links, links to the current page, or red links
 # Stopping when reaching "Philosophy", a page with no links or a page that does not exist, or when a loop occurs
 
-# site=$1
-# curl -s $initialSite > temp
-# initialSiteTitle=$(cat temp | grep --color=never "<title>.*</title>" | sed "s/<title>//" | sed "s/<\/title>//" | sed "s|[[:space:]]\-[[:space:]]Wikipedia||" | tr -d " ")
-
 waitExpression=0
 count=0
 
+curl -s $1 > temp
+startingTitle=$(cat temp | grep --color=never "<title>.*</title>" | sed "s/<title>//" | sed "s/<\/title>//" | sed "s|[[:space:]]\-[[:space:]]Wikipedia||")
+
 green='\033[0;32m'
+blue='\033[0;34m'
 noColor='\033[0m'
 
+printGreen() {
+  echo ${green}$1${noColor}
+}
+
+printBlue() {
+  echo ${blue}$1${noColor}
+}
+
+printSuccessMessage() {
+  currentArticle=$1
+  steps=$2
+  echo -e "\nArrival!  Took $(printGreen $steps) steps to get from $(printGreen $startingTitle) to $(printBlue $currentArticle) \n"
+}
+
 getNextArticle() {
-  echo "*************"
-  site=$1
+  echo -e "\n*************"
+  site="$1"
+  count=$2
+
   curl -s $site > temp
-  title=$(cat temp | grep --color=never "<title>.*</title>" | sed "s/<title>//" | sed "s/<\/title>//" | sed "s|[[:space:]]\-[[:space:]]Wikipedia||" | tr -d " ")
+  title=$(cat temp | grep --color=never "<title>.*</title>" | sed "s/<title>//" | sed "s/<\/title>//" | sed "s|[[:space:]]\-[[:space:]]Wikipedia||")
 
-  echo -e "Current article is ${green}$title${noColor} at address ${green}$site${noColor}"
+  [[ ${title} == "Philosophy"  ]] && { printSuccessMessage $title $count; exit 0; }
 
-  [[ ${title} == "Philosophy"  ]] && { echo "Found it!"; exit 0; }
+  echo -e "Current article is $(printGreen "$title") \nthis is stop number $(printGreen $count)"
 
-  cat temp | grep --color=never "<p>.*" > temp2
+  cat temp | grep --color=never "<p>.*" | sed "s|[[:space:]]([^)]*)||g" > temp2
   firstLink=$(cat temp2 | grep --color=never -o -m 1 "href=\"/wiki/.*\"" )
 
   stringarray=($firstLink)
@@ -33,29 +49,9 @@ getNextArticle() {
 
   nextPage="https://en.wikipedia.org$strippedLink"
 
-  echo -e "\nnext page is ${green}$nextPage${noColor}"
-  #
-  getNextArticle $nextPage
+  echo -e "\nnext stop is $(printBlue $nextPage)"
+  let count=count+1
+  getNextArticle $nextPage $count
 }
 
-getNextArticle $1
-
-# site=$1
-# curl -s $site > temp
-# title=$(cat temp | grep --color=never "<title>.*</title>" | sed "s/<title>//" | sed "s/<\/title>//" | sed "s|[[:space:]]\-[[:space:]]Wikipedia||" | tr -d " ")
-# cat temp | grep "<p>.*" > temp2
-# firstLink=$(cat temp2 | grep -o -m 1 "href=\"/wiki/.*\"" )
-# # sedStyle=$(sed -n "s|href=\"/wiki/[a-zA-Z\_\-]*\"|\1|p" temp2)
-#
-# echo $title
-# [[ ${title} == "Quantity" ]] && Echo True
-#
-# stringarray=($firstLink)
-# realFirstLink=$(echo ${stringarray[0]})
-#
-# echo $realFirstLink
-# strippedLink=$(echo $realFirstLink | sed "s/href=\"//" | sed "s/\"//")
-# echo $strippedLink
-#
-# nextPage="https://en.wikipedia.org$strippedLink"
-# echo $nextPage
+getNextArticle $1 0
