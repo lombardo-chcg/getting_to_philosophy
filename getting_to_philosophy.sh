7982ef8526438d506008914225de769797f4e790
lombardo-chcg <dpa.lombardo@gmail.com>
2017-02-05 11:01:15 -0600

    basic recursive function workining but edge cases persist

--- getting_to_philosophy.sh
+++ getting_to_philosophy.sh
@@ -4,20 +4,58 @@
 # Ignoring external links, links to the current page, or red links
 # Stopping when reaching "Philosophy", a page with no links or a page that does not exist, or when a loop occurs
 
-site=$1
-curl -s $site > temp
-title=$(cat temp | grep "<title>.*</title>" | sed "s/<title>//" | sed "s/<\/title>//" | sed "s/[[:space:]]\-[[:space:]]Wikipedia//")
-cat temp | grep "<p>.*" > temp2
-firstLink=$(cat temp2 | grep -o -m 1 "href=\"/wiki/.*\"" )
-# sedStyle=$(sed -n "s|href=\"/wiki/[a-zA-Z\_\-]*\"|\1|p" temp2)
+# site=$1
+# curl -s $initialSite > temp
+# initialSiteTitle=$(cat temp | grep --color=never "<title>.*</title>" | sed "s/<title>//" | sed "s/<\/title>//" | sed "s|[[:space:]]\-[[:space:]]Wikipedia||" | tr -d " ")
 
-echo $title
-stringarray=($firstLink)
-realFirstLink=$(echo ${stringarray[0]})
+waitExpression=0
+count=0
 
-echo $realFirstLink
-strippedLink=$(echo $realFirstLink | sed "s/href=\"//" | sed "s/\"//")
-echo $strippedLink
+green='\033[0;32m'
+noColor='\033[0m'
 
-nextPage="https://en.wikipedia.org$strippedLink"
-echo $nextPage
+getNextArticle() {
+  echo "*************"
+  site=$1
+  curl -s $site > temp
+  title=$(cat temp | grep --color=never "<title>.*</title>" | sed "s/<title>//" | sed "s/<\/title>//" | sed "s|[[:space:]]\-[[:space:]]Wikipedia||" | tr -d " ")
+
+  echo -e "Current article is ${green}$title${noColor} at address ${green}$site${noColor}"
+
+  [[ ${title} == "Philosophy"  ]] && { echo "Found it!"; exit 0; }
+
+  cat temp | grep --color=never "<p>.*" > temp2
+  firstLink=$(cat temp2 | grep --color=never -o -m 1 "href=\"/wiki/.*\"" )
+
+  stringarray=($firstLink)
+  realFirstLink=$(echo ${stringarray[0]})
+  strippedLink=$(echo $realFirstLink | sed "s/href=\"//" | sed "s/\"//")
+
+  nextPage="https://en.wikipedia.org$strippedLink"
+
+  echo -e "\nnext page is ${green}$nextPage${noColor}"
+  #
+  getNextArticle $nextPage
+}
+
+getNextArticle $1
+
+# site=$1
+# curl -s $site > temp
+# title=$(cat temp | grep --color=never "<title>.*</title>" | sed "s/<title>//" | sed "s/<\/title>//" | sed "s|[[:space:]]\-[[:space:]]Wikipedia||" | tr -d " ")
+# cat temp | grep "<p>.*" > temp2
+# firstLink=$(cat temp2 | grep -o -m 1 "href=\"/wiki/.*\"" )
+# # sedStyle=$(sed -n "s|href=\"/wiki/[a-zA-Z\_\-]*\"|\1|p" temp2)
+#
+# echo $title
+# [[ ${title} == "Quantity" ]] && Echo True
+#
+# stringarray=($firstLink)
+# realFirstLink=$(echo ${stringarray[0]})
+#
+# echo $realFirstLink
+# strippedLink=$(echo $realFirstLink | sed "s/href=\"//" | sed "s/\"//")
+# echo $strippedLink
+#
+# nextPage="https://en.wikipedia.org$strippedLink"
+# echo $nextPage
